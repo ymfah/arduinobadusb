ce5eb94412b4d3926520a5e63b899754f9bfd419
ymfah <44611140+ymfah@users.noreply.github.com>
2021-09-17 02:47:44 +0900

    Add files via upload

---
+++ 1.Digispark/Benchmark/100/Benchmark/Benchmark.ino
@@ -0,0 +1,13 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+
+}
+
+
+void loop() {
+
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.println("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m");
+
+}
---
+++ 2.Leonardo/benchmark/100/100.ino
@@ -0,0 +1,9 @@
+ #include "Keyboard.h"  
+ void setup() {  
+  Keyboard.begin();  
+ }  
+ void loop() {  
+  delay(0);  
+  Keyboard.println("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean m");  
+ }  
+ 
